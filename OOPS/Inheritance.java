09ec2bdec71477ba2a36cd52c02922bef70f06cc
Mohitraj27 <raj716980@gmail.com>
2023-09-01 03:22:14 +0530

    Inheritance

---
+++ OOPS/Inheritance.java
@@ -0,0 +1,54 @@
+/*
+ Inheritance is a process of obtaining the data members and methods from one class to another class plus have its own is known as inheritance.
+
+ Subclass: child class/ derived class / inherited class  a class that inherites the other class 
+
+ Super class is the parent class  whose features are inherited also known as base class 
+
+ Eg. all properties of a father is inherited by a son
+ */
+package OOPS;
+
+public class Inheritance {
+    public static void main(String[] args) {
+        Animal dog = new Dog();
+        dog.setId(123); // Inherited from the super class
+        dog.sound();
+    }
+}
+
+/*
+ * This is the parent class (also called as super class or base class)
+ */
+class Animal {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void sound() {
+        System.out.println("By default, it is mute");
+    }
+}
+
+/*
+ * This is the child class / subclass which is Dog that inherits the properties of class Animal
+ */
+class Dog extends Animal {
+
+    // Own behavior
+    private void bark() {
+        System.out.println("Dog " + getId() + " is barking");
+    }
+
+    // Override super class/parent class behavior
+    @Override
+    public void sound() {
+        bark();
+    }
+}
